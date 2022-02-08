8d436ab831962c99ce68d2b2180b1f06bda07df2
yovafeng <64761294@qq.com>
2022-02-08 15:49:19 +0800

    minor: 新增core模块

---
+++ core/flowtmpl/tree_example.py
@@ -0,0 +1,17 @@
+"""
+流程模板样例
+"""
+[
+    {
+        "name": "节点name",
+        "description": "节点描述",
+        "component_code": "插件code",
+        "version": "插件版本",
+    },
+    {
+        "name": "节点name",
+        "description": "节点描述",
+        "component_code": "插件code",
+        "version": "插件版本",
+    }
+]
